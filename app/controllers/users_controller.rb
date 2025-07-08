149986fa04706743c137d4fd3116ac5d69016f55
Yasueda <217292364+Yasueda@users.noreply.github.com>
2025-07-08 04:49:01 +0000

    [Finish] access_control

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,20 +22,21 @@ class UsersController < ApplicationController
     if @user.update(user_params)
       flash[:notice] = "You have updated user successfully."
       redirect_to user_path(@user.id)
+    else
+      render :edit
     end
-    render :edit
   end
 
   private
 
   def user_params
-    params.require(:user).permit(:name, :introduction, :image)
+    params.require(:user).permit(:name, :introduction, :profile_image)
   end
 
   def is_matching_login_user
     user = User.find(params[:id])
     unless user.id == current_user.id
-      redirect_to user_path(params[:id])
+      redirect_to user_path(current_user.id)
     end
   end
 
